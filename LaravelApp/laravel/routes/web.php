24d7610b03441ec68fd758cacd00724933a8b878
Masayuki Ikeda <masayuki.ikeda@connec10.co.jp>
2021-09-12 20:17:15 +0900

    足りなかった情報を追加

--- LaravelApp/laravel/routes/web.php
+++ LaravelApp/laravel/routes/web.php
@@ -12,7 +12,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Top
+//Route::get('/', 'HomeController@index')->name('home');
+
+// folder関連
+Route::get('/folders/new', 'FolderController@new')->name('folders.new');
+Route::post('/folders/create', 'FolderController@create')->name('folders.create');
+
+// task関連
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/{id}/tasks/new', 'TaskController@new')->name('tasks.new');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create')->name('tasks.create');
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/update', 'TaskController@update')->name('tasks.update');
+
+// Auth
+//Auth::routes();
